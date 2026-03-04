0656f1e68f9dce6362a237b9fbccbd551b10b8bc
arelghost777 <arelghost@gmail.com>
2026-03-04 12:41:01 +0100

    Add empty object as second argument to translation calls

--- src/app/[locale]/projects/[id]/page.tsx
+++ src/app/[locale]/projects/[id]/page.tsx
@@ -20,11 +20,12 @@ export default async function ProjectPage({ params }: { params: Params }) {
   const t = await getI18n();
 
   const project = {
-    ...rawProject,
-    title: t(`projects.items.${rawProject!.id}.title` as any),
-    description: t(`projects.items.${rawProject!.id}.description` as any),
-    overview: t(`projects.items.${rawProject!.id}.overview` as any),
-  };
+  ...rawProject,
+  title: t(`projects.items.${rawProject!.id}.title` as any, {} as any),
+  description: t(`projects.items.${rawProject!.id}.description` as any, {} as any),
+  overview: t(`projects.items.${rawProject!.id}.overview` as any, {} as any),
+};
+
 
   // Extraire la première phrase pour la citation
   const firstSentence = project.overview.split('.')[0] + '.';
